82f38dba549e9a74092a9439284dce43ade4e771
programmeramandeep <programmeramandeep@gmail.com>
2021-05-10 18:35:31 +0530

    setup tailwind css

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import FirebaseContext from "../context/firebase";
 
 const Login = () => {
-    return <div>I am the login page</div>;
+    const history = useHistory();
+    const { firebase } = useContext(FirebaseContext);
+
+    const [emailAddress, setEmailAddress] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [error, setError] = useState("");
+    const isInvalid = password === "" || emailAddress === "";
+
+    const handleLogin = () => {
+        //
+    };
+
+    useEffect(() => {
+        document.title = "Login - Instagram";
+    }, []);
+
+    return <div className="container">I am the login page</div>;
 };
 
 export default Login;
