40e6c488d25f5225facb27caf1b00aed1acd6fa5
programmeramandeep <programmeramandeep@gmail.com>
2021-05-11 15:49:57 +0530

    added user context

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
+import UseAuthListener from "./hooks/UseAuthListener";
+import UserContext from "./context/user";
 
 const Login = lazy(() => import("./pages/Login"));
 const SignUp = lazy(() => import("./pages/SignUp"));
 const NotFound = lazy(() => import("./pages/NotFound"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
 
 const App = () => {
+    const { user } = UseAuthListener();
+
     return (
-        <Router>
-            <Suspense fallback={<p>Loading...</p>}>
-                <Switch>
-                    <Route path={ROUTES.LOGIN} component={Login} exact />
-                    <Route path={ROUTES.SIGN_UP} component={SignUp} exact />
-                    <Route component={NotFound} />
-                </Switch>
-            </Suspense>
-        </Router>
+        <UserContext.Provider value={{ user }}>
+            <Router>
+                <Suspense fallback={<p>Loading...</p>}>
+                    <Switch>
+                        <Route path={ROUTES.LOGIN} component={Login} exact />
+                        <Route path={ROUTES.SIGN_UP} component={SignUp} exact />
+                        <Route
+                            path={ROUTES.DASHBOARD}
+                            component={Dashboard}
+                            exact
+                        />
+                        <Route component={NotFound} />
+                    </Switch>
+                </Suspense>
+            </Router>
+        </UserContext.Provider>
     );
 };
 
