0c63ebbbfbf5dd117d9c3bba310d9f2f2209a9c5
programmeramandeep <programmeramandeep@gmail.com>
2021-05-13 15:49:34 +0530

    protected routes

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import * as ROUTES from "./constants/routes";
 import UseAuthListener from "./hooks/UseAuthListener";
 import UserContext from "./context/user";
 
+import ProtectedRoute from "./helpers/ProtectedRoute";
+import IsUserLoggedIn from "./helpers/IsUserLoggedIn";
+
 const Login = lazy(() => import("./pages/Login"));
 const SignUp = lazy(() => import("./pages/SignUp"));
 const NotFound = lazy(() => import("./pages/NotFound"));
 const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Profile = lazy(() => import("./pages/Profile"));
 
 const App = () => {
     const { user } = UseAuthListener();
@@ -17,13 +21,31 @@ const App = () => {
             <Router>
                 <Suspense fallback={<p>Loading...</p>}>
                     <Switch>
-                        <Route path={ROUTES.LOGIN} component={Login} exact />
-                        <Route path={ROUTES.SIGN_UP} component={SignUp} exact />
-                        <Route
+                        <IsUserLoggedIn
+                            user={user}
+                            loggedInPath={ROUTES.DASHBOARD}
+                            path={ROUTES.LOGIN}
+                        >
+                            <Login />
+                        </IsUserLoggedIn>
+
+                        <IsUserLoggedIn
+                            user={user}
+                            loggedInPath={ROUTES.DASHBOARD}
+                            path={ROUTES.SIGN_UP}
+                        >
+                            <SignUp />
+                        </IsUserLoggedIn>
+
+                        <Route path={ROUTES.PROFILE} component={Profile} />
+
+                        <ProtectedRoute
+                            user={user}
                             path={ROUTES.DASHBOARD}
-                            component={Dashboard}
                             exact
-                        />
+                        >
+                            <Dashboard />
+                        </ProtectedRoute>
                         <Route component={NotFound} />
                     </Switch>
                 </Suspense>
